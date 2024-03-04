aa5634c9569a9fe4e3552c6c50d7780bea7e2fb6
Josue Ramirez <84062963+josueramirez1@users.noreply.github.com>
2024-03-03 18:20:26 -0800

    added hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const sections = document.querySelectorAll("section, header, footer");
+const navItems = document.querySelector(".nav-items");
+const topHeader = document.querySelector(".top-header");
 
+const header = document.querySelector("#header");
+const navBtn = document.querySelector(".btn-mobile-nav");
 const options = {
   threshold: 0,
   rootMargin: "-50px",
 };
+
+// Navigation menu
+navBtn.addEventListener("click", (e) => {
+  if (!navBtn) return;
+  header.classList.toggle("nav-open");
+  navItems.addEventListener("click", (e) => {
+    header.classList.remove("nav-open");
+  });
+});
+
+// Scrolling
 
 const observer = new IntersectionObserver((entries) => {
   for (let entry of entries) {
@@ -14,6 +29,27 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+// Sticky navigation
+// const sticky = new IntersectionObserver(
+//   (entries) => {
+//     const ent = entries[0];
+//     if (!ent.isIntersecting) {
+//       document.body.classList.add("sticky");
+//     }
+
+//     if (ent.isIntersecting) {
+//       document.body.classList.remove("sticky");
+//       console.log(ent);
+//     }
+//   },
+//   {
+//     threshold: 0,
+//     root: null,
+//     rootMargin: "-105px",
+//   }
+// );
+// sticky.observe(topHeader);
 
 //Thank you HTML page
 const redirect = "index.html";
