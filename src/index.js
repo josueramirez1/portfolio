c217726f513ddbd982f96a7f629ecce64040049a
josueramirez1 <josue67ramirez@gmail.com>
2024-08-26 15:19:02 -0700

    adding new file and changing names to some script files.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import items from "./items.js";
+import successPage from "./thankYou.js";
+import "./css/styles.css";
+import "./css/general.css";
+import "./css/queries.css";
+
+items();
+successPage();
