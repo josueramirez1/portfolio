d10aefb50b0b8af48ef3ff0e45640f3af29c5d9a
josueramirez1 <josue67ramirez@gmail.com>
2024-08-27 12:56:28 -0700

    removing dist

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
     ],
